5078c860a657a797260b5382b2388bd235246660
Tamas <demeny_tamas@yahoo.com>
2020-03-25 12:21:34 +0100

    some sorts try output

--- src/hu/tamasdemeny/customers/Customer.java
+++ src/hu/tamasdemeny/customers/Customer.java
@@ -11,7 +11,17 @@ public class Customer {
     public static void main(String[] args) {
         List fullData = new ArrayList();
         fullData = reader();
-        System.out.println(fullData);
+        //System.out.println(fullData);
+
+        //Set s = new TreeSet();
+        //System.out.println("Név szerint ABC: ");
+        Collections.sort(fullData, new OsszehasonlitasNevSzerint());
+        //System.out.println(fullData);
+
+        //System.out.println("Összeg szerint: ");
+        Collections.sort(fullData, new OsszehasonlitasOsszegSzerint());
+        //System.out.println(fullData);
+        writer(fullData);
     }
 
     static List reader() {
@@ -21,11 +31,14 @@ public class Customer {
         try {
             br = new BufferedReader(new FileReader(new File("src/hu/tamasdemeny/customers/ugyfelek.csv")));
             String line;
-
+            line = br.readLine();
             while ((line = br.readLine()) != null) {
                 //System.out.println(line);
                 String[] newData = line.split(",");
-                fullData.add(new OneCustomer(newData[0], newData[1],newData[2],newData[3],newData[4],newData[5],newData[6],newData[7]));
+                Integer balance = Integer.parseInt(newData[6]);
+                Integer saved = Integer.parseInt(newData[7]);
+                System.out.println("balance + saved: " + balance +  saved);
+                fullData.add(new OneCustomer(newData[0], newData[1],newData[2],newData[3],newData[4],newData[5],balance, saved));
             }
 
         }   catch (FileNotFoundException fnfe) {
@@ -43,9 +56,46 @@ public class Customer {
     return fullData;
     }
 
+    static void writer(List fullData) {
 
 
-}
+        File f = new File("penzosszegek.csv");
+
+        FileWriter fw = null;
+        try {
+            fw = new FileWriter(f);
+
+            for (int i=1;i<fullData.size();i++) {
+                String currentLine = fullData.get(i).toString();
+                System.out.println("Line " + i + " " + currentLine);
+                fw.append(currentLine);
+                fw.append("\n");
+
+                if (i % 10 == 0) {
+                    fw.flush();
+                }
+            }
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        } finally {
+
+            try {
+                fw.flush();
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+
+        }
+    }
+
+
 
 
 class OneCustomer implements Comparable {
@@ -56,8 +106,8 @@ class OneCustomer implements Comparable {
     private String placeBirth;
     private String accountDate;
     private String accountNum;
-    private String accountBalance;
-    private String accountSaved;
+    private Integer accountBalance;
+    private Integer accountSaved;
 
 
     @Override
@@ -70,7 +120,7 @@ class OneCustomer implements Comparable {
         return 0;
     }
 
-    public OneCustomer(String name, String motherName,String dob, String placeBirth, String accountDate,String accountNum,String accountBalance, String accountSaved  ) {
+    public OneCustomer(String name, String motherName,String dob, String placeBirth, String accountDate,String accountNum,Integer accountBalance, Integer accountSaved  ) {
         this.name = name;
         this.motherName = motherName;
         this.dob = dob;
@@ -80,6 +130,69 @@ class OneCustomer implements Comparable {
         this.accountBalance = accountBalance;
         this.accountSaved = accountSaved;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAccountBalance() {
+        return accountBalance;
+    }
+
+    public Integer getAccountSaved() {
+        return accountSaved;
+    }
+}
+
+
+
+class OsszehasonlitasNevSzerint implements Comparator {
+
+    @Override
+    public int compare(Object o1, Object o2) {
+
+        OneCustomer p1 = (OneCustomer) o1;
+        OneCustomer p2 = (OneCustomer) o2;
+
+
+        if (p1.getName() == p2.getName()) {
+            return 0;
+        } else {
+            if (p1.getName().compareTo(p2.getName())> 0){
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+
+    }
+
+
+}
+
+class OsszehasonlitasOsszegSzerint implements Comparator {
+
+    @Override
+    public int compare(Object o1, Object o2) {
+
+        OneCustomer p1 = (OneCustomer) o1;
+        OneCustomer p2 = (OneCustomer) o2;
+        Integer total1 =p1.getAccountBalance() + p1.getAccountSaved();
+        Integer total2 =p2.getAccountBalance() + p2.getAccountSaved();
+
+        if (total1 == total2) {
+            return 0;
+        } else {
+            if (total1>total2){
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+
+    }
+
+
 }
 
 
